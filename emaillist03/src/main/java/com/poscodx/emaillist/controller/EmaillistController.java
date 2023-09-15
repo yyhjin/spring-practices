569fa8a9e871e7b89738e8f4093d1471ac846d09
yyhjin <wendy0301@naver.com>
2023-09-15 12:00:28 +0900

    emaillist03 #2

--- emaillist03/src/main/java/com/poscodx/emaillist/controller/EmaillistController.java
+++ emaillist03/src/main/java/com/poscodx/emaillist/controller/EmaillistController.java
@@ -1,15 +1,21 @@
 package com.poscodx.emaillist.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.poscodx.emaillist.repository.EmaillistRepository;
+
 @Controller
 public class EmaillistController {
+	@Autowired
+	private EmaillistRepository emaillistRepository;
 	
 	@ResponseBody
 	@RequestMapping("/")
 	public String main() {
+		emaillistRepository.findAll();
 		return "OK!";
 	}
 	
---
+++ emaillist03/src/main/java/com/poscodx/emaillist/repository/EmaillistRepository.java
@@ -0,0 +1,164 @@
+package com.poscodx.emaillist.repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.poscodx.emaillist.vo.EmaillistVo;
+
+@Repository
+public class EmaillistRepository {
+
+	public List<EmaillistVo> findAll() {
+		List<EmaillistVo> result = new ArrayList<>();
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = getConnection();
+			
+			String sql =
+					"select no, first_name, last_name, email "
+					+ "from emaillist "
+					+ "order by no asc";
+			pstmt = conn.prepareStatement(sql);
+			
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				Long no = rs.getLong(1);
+				String firstName = rs.getString(2);
+				String lastName = rs.getString(3);
+				String email = rs.getString(4);
+				
+				EmaillistVo vo = new EmaillistVo();
+				vo.setNo(no);
+				vo.setFirstName(firstName);
+				vo.setLastName(lastName);
+				vo.setEmail(email);
+				
+				result.add(vo);
+			}
+			
+		} catch (SQLException e) {
+			System.out.println("error: " + e);
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return result;
+	}
+
+	
+	public void insert(EmaillistVo vo) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = getConnection();
+			
+			String sql =
+					"insert into emaillist "
+					+ "values(null, ?, ?, ?)";
+			pstmt = conn.prepareStatement(sql);
+				
+
+			pstmt.setString(1, vo.getFirstName());
+			pstmt.setString(2, vo.getLastName());
+			pstmt.setString(3, vo.getEmail());
+			
+			rs = pstmt.executeQuery();
+			
+		} catch (SQLException e) {
+			System.out.println("error: " + e);
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+
+	public void deleteByEmail(String email) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = getConnection();
+			
+			String sql =
+					"delete from emaillist "
+					+ "where email = ?";
+			pstmt = conn.prepareStatement(sql);
+				
+			pstmt.setString(1, email);
+			
+			rs = pstmt.executeQuery();
+			
+		} catch (SQLException e) {
+			System.out.println("error: " + e);
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+	private Connection getConnection() throws SQLException {
+		Connection conn = null;
+			
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			String url = "jdbc:mariadb://192.168.0.175:3307/webdb?charset=utf8";
+			conn = DriverManager.getConnection(url, "webdb", "webdb123");
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패 : " + e);
+		} 
+
+		return conn;
+	}
+	
+}
